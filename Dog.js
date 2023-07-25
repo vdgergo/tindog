1a60be4bb293163bc91f2b6b52bdc18ba6ceb6b1
Varga-Dudás Gergő <vargadgergo@gmail.com>
2023-07-25 20:46:25 +0200

    html and css ready

---
+++ Dog.js
@@ -0,0 +1,10 @@
+// Create the Dog class here
+
+class Dog{
+  constructor(data){
+  }
+}
+
+
+const valami = "teszt"
+console.log(valami)
---
+++ index.js
@@ -0,0 +1,4 @@
+import { dogs } from "./data.js";
+
+console.log(dogs)
+console.log('dogs', dogs)
