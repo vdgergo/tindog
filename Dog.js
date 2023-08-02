1e2713e391fc789e643a2218690dd5c750dbe02d
Varga-Dudás Gergő <vargadgergo@gmail.com>
2023-08-02 18:52:28 +0200

    first version ready

--- Dog.js
+++ Dog.js
@@ -1,10 +1,31 @@
 // Create the Dog class here
 
-class Dog{
-  constructor(data){
+class Dog {
+  constructor(data) {
+    Object.assign(this,data)
+    this.domElement = document.getElementById('dog-container')
+    this.renderDogHTML()
   }
+
+  renderDogHTML(){
+    // <img class="dog__badge" src="images/badge-like.png" alt=""></img>
+    const {name, avatar, age, bio} = this
+    this.domElement.innerHTML = `
+          <div id="dog" class="dog">
+            <div class="description-wrapper">
+              <h1 class="dog__name">${name},<span class="dog__age">${age}</span</h1>
+              <p class="dog__bio">${bio}</p>
+            </div>
+          </div>
+          <div class="input-wrapper">
+            <button id="decline-btn" class="input-btn input-btn__decline input-btn__decline"><img src="images/icon-cross.png" alt=""></button>
+            <button id="accept-btn" class="input-btn input-btn__accept input-btn__accept"><img src="images/icon-heart.png" alt=""></button>
+          </div>`
+    document.getElementById('dog').style.backgroundImage = `url(${avatar})`
+  }
+
 }
 
 
-const valami = "teszt"
-console.log(valami)
+
+export default Dog
--- index.js
+++ index.js
@@ -1,4 +1,51 @@
 import { dogs } from "./data.js";
+import Dog from "./Dog.js";
 
-console.log(dogs)
-console.log('dogs', dogs)
+const dogElement = document.getElementById('dog')
+
+const dogsArray = ["Rex","Bella","Teddy"]
+// const rex = new Dog(dogs[2])
+// rex.renderDogHTML()
+
+
+function getNewDog() {
+  const newDogName = dogsArray.shift()
+  const nextDogData = dogs.filter(function(item){
+    return item.name === newDogName
+  })[0]
+  return nextDogData ? new Dog(nextDogData) : false
+}
+
+function render(){
+  dog.renderDogHTML()
+  document.getElementById("decline-btn").addEventListener("click", swipeLeft)
+  document.getElementById("accept-btn").addEventListener("click", swipeRight)
+}
+
+function swipeLeft(){
+  dog.hasBeenSwiped = true;
+  dog.hasBeenLiked = false
+  console.log("nope")
+  document.getElementById("dog").insertAdjacentHTML("afterbegin",`<img class="dog__badge" src="images/badge-nope.png" alt=""></img>`)
+  dog = getNewDog()
+  console.log(dog)
+  if (dog) {
+    setTimeout(render,1500)
+  }
+  else {document.getElementById("dog-container").innerText = "No more single dogs around you!"}
+
+  
+}
+
+function swipeRight(){
+  dog.hasBeenSwiped = true;
+  dog.hasBeenLiked = true;
+  console.log("like")
+  document.getElementById("dog").insertAdjacentHTML("afterbegin",`<img class="dog__badge" src="images/badge-like.png" alt=""></img>`)
+  dog = getNewDog()
+  setTimeout(render,1500)
+}
+
+
+let dog = getNewDog()
+render()
